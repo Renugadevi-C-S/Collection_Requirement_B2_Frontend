6436a9232183baf1873f78a0a243695092d44327
Anbazhagan, Raamji(R.) <ranbazh3@ford.com>
2025-10-23 19:33:46 +0530

    Add lc-dashboard layout with navigation and routing; include request form and requests list components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,18 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component'; // Import your component
 import { LcDashboardComponent } from './components/lc-dashboard/lc-dashboard.component';
 import { LdspocDashboardComponent } from './components/ldspoc-dashboard/ldspoc-dashboard.component';
+import { LcRequestFormComponent } from './components/lc-request-form/lc-request-form.component';
+import { LcRequestsListComponent } from './components/lc-requests-list/lc-requests-list.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'lc-dashboard', component: LcDashboardComponent },
+  { path: 'lc-dashboard',
+    component: LcDashboardComponent,
+    children: [
+          { path: 'submit-request', component: LcRequestFormComponent },
+          { path: 'view-requests', component: LcRequestsListComponent }
+        ]
+   },
   { path: 'ldspoc-dashboard', component: LdspocDashboardComponent },
-  // ... other routes (e.g., for dashboard)
 ];
--- src/app/components/lc-dashboard/lc-dashboard.component.ts
+++ src/app/components/lc-dashboard/lc-dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-lc-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './lc-dashboard.component.html',
   styleUrl: './lc-dashboard.component.css'
 })
