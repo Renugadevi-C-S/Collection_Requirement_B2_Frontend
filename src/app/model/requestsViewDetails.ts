36c3a74a704167237656b4bca244bf4331204bb4
Anbazhagan, Raamji(R.) <ranbazh3@ford.com>
2025-10-25 16:09:17 +0530

    added requests details model similar to response sent from  backend

---
+++ src/app/model/requestsViewDetails.ts
@@ -0,0 +1,9 @@
+export interface requestsViewDetails {
+    requestId: number;
+    department: string;
+    eventName: string;
+    requestStatus: string;
+    requestDate: Date | string;
+    noOfParticipants: number;
+    justification: string;
+}
