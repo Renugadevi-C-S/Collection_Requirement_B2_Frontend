cf13e3c891d52e9239157b295b11ae42b197f3f6
Anbazhagan, Raamji(R.) <ranbazh3@ford.com>
2025-10-25 12:46:50 +0530

    Refactor header component to use async pipe for loggedInUser and improve user session management

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { UserService } from '../../sevices/user.service';
+import { LoginResponse } from '../../model/logInResponse';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -10,16 +12,26 @@ import { UserService } from '../../sevices/user.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent  {
+export class HeaderComponent implements OnInit {
   
-  loggedInUser: any;
+  loggedInUser: Observable<LoginResponse | null>;
+  currentUser: LoginResponse | null = null;
 
   constructor(private router: Router, private userService: UserService) {
     this.loggedInUser = userService.loggedInUser;
   }
+  ngOnInit(): void {
+    this.loggedInUser.subscribe(user => {
+      this.currentUser = user;
+    });
+  }       
+    
+  
 
   logout(): void {
-    this.userService.clearLoggedInUser();
-    this.router.navigate(['/login']);
+    if(window.confirm(`${this.currentUser?.firstName}, want to LogOut?`)){
+      this.userService.clearLoggedInUser();
+      this.router.navigate(['/login']);
+    }
   }
 }
