7c2ab0b2b3a33f1d721cc1410b52637d236b22ee
Anbazhagan, Raamji(R.) <ranbazh3@ford.com>
2025-10-25 16:52:55 +0530

    removed contents of lDSPOC dashboard

--- src/app/components/ldspoc-dashboard/ldspoc-dashboard.component.ts
+++ src/app/components/ldspoc-dashboard/ldspoc-dashboard.component.ts
@@ -1,38 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { UserService } from '../../sevices/user.service';
-import { LoginResponse } from '../../model/logInResponse';
-import { Subscription } from 'rxjs';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-ldspoc-dashboard',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './ldspoc-dashboard.component.html',
   styleUrl: './ldspoc-dashboard.component.css'
 })
-export class LdspocDashboardComponent implements OnInit, OnDestroy {
+export class LdspocDashboardComponent {
   
-  currentUser: LoginResponse | null = null;
-  private userSubscription?: Subscription;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit(): void {
-    this.userSubscription = this.userService.loggedInUser.subscribe(user => {
-      this.currentUser = user;
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.userSubscription) {
-      this.userSubscription.unsubscribe();
-    }
-  }
-
-  getUserDisplayName(): string {
-    if (this.currentUser) {
-      return `${this.currentUser.firstName} ${this.currentUser.lastName}`;
-    }
-    return 'User';
-  }
 }
