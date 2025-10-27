38477025c170d6311acf896e488b7e251f90febe
MPRADE11 <pradeepasundar115@gmail.com>
2025-10-27 20:22:55 +0530

    implemented l&dspoc dashboard features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,49 +4,36 @@ import { LcDashboardComponent } from './components/lc-dashboard/lc-dashboard.com
 import { LdspocDashboardComponent } from './components/ldspoc-dashboard/ldspoc-dashboard.component';
 import { LcRequestFormComponent } from './components/lc-request-form/lc-request-form.component';
 import { LcRequestsListComponent } from './components/lc-requests-list/lc-requests-list.component';
-<<<<<<< HEAD
 import { LdspocRequestFormComponent } from './components/ldspoc-request-form/ldspoc-request-form.component';
 import { LdspocRquestsListComponent } from './components/ldspoc-rquests-list/ldspoc-rquests-list.component';
 import { LdspocEventFormComponent } from './components/ldspoc-event-form/ldspoc-event-form.component';
 import { LdspocEventListComponent } from './components/ldspoc-event-list/ldspoc-event-list.component';
-=======
 import { authGuard, roleGuard } from './guards/auth.guard';
->>>>>>> origin/raamji
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { 
+  {
     path: 'lc-dashboard',
     component: LcDashboardComponent,
     canActivate: [authGuard, roleGuard],
-    data: { roles: ['LC','ADMIN'] },
+    data: { roles: ['LC', 'ADMIN'] },
     children: [
-<<<<<<< HEAD
-          { path: 'submit-request', component: LcRequestFormComponent },
-          { path: 'view-requests', component: LcRequestsListComponent }
-        ]
-   },
-  { path: 'ldspoc-dashboard', component: LdspocDashboardComponent ,
-    children: [
-          { path: 'submit-request', component: LdspocRequestFormComponent },
-          { path: 'view-requests', component: LdspocRquestsListComponent },
-          { path: 'create-event', component: LdspocEventFormComponent },
-          { path: 'view-events', component: LdspocEventListComponent }
-        ]
-  },
-=======
       { path: 'submit-request', component: LcRequestFormComponent },
       { path: 'view-requests', component: LcRequestsListComponent }
     ]
   },
-  { 
-    path: 'ldspoc-dashboard', 
+  {
+    path: 'ldspoc-dashboard',
     component: LdspocDashboardComponent,
     canActivate: [authGuard, roleGuard],
-    data: { roles: ['LNDSPOC','ADMIN'] }
+    data: { roles: ['LNDSPOC', 'ADMIN'] },
+    children: [
+      { path: 'submit-request', component: LdspocRequestFormComponent },
+      { path: 'view-requests', component: LdspocRquestsListComponent },
+      { path: 'create-event', component: LdspocEventFormComponent },
+      { path: 'view-events', component: LdspocEventListComponent }
+    ]
   },
   { path: '**', redirectTo: '/login' }
->>>>>>> origin/raamji
 ];
-
