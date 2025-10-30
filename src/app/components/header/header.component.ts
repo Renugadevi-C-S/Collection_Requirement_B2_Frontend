f44783e3bfaf52c2f31710a389a0cc7cd1e19a13
Anbazhagan <ranbazh3@ford.com>
2025-10-30 12:30:03 +0530

    Added logo to header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -14,6 +14,7 @@ import { Subscription } from 'rxjs';
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   
+  fordLogo = 'ford-logo.png';
   currentUser: LoginResponse | null = null;
   private userSubscription?: Subscription;
 
