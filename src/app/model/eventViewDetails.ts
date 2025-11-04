d4aba5ba9335af8868c586c24660c125178895cd
C S, Renugadevi (R.) <rcs3@ford.com>
2025-11-04 10:40:24 +0530

    Added missing fields and interface for linked requests summary.

--- src/app/model/eventViewDetails.ts
+++ src/app/model/eventViewDetails.ts
@@ -1,8 +1,27 @@
 export interface EventViewDetails {
   eventId: number;
   eventName: string;
+  description: string;
   duration: number;
   eventType: string;
+  fundingSource: string;
+  participantsCount: number;
   status: string;
   createdBy: string;
+  linkedRequests?: LinkedRequestSummary[];
+}
+
+//Interface for linked requests summary
+export interface LinkedRequestSummary {
+  requestId: number;
+  tanNo: string;
+  noOfParticipants: number;
+  requestDate: string;
+  justification: string;
+  requestedBy: string;
+  department: string;
+  requestStatus: string;
+  curriculum: string;
+  approvedBy: string;
+  approvalNotes: string;
 }
