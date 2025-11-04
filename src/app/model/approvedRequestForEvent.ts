f01ee784eb6964f5fcc335181f4ff119b955f0f8
C S, Renugadevi (R.) <rcs3@ford.com>
2025-11-04 20:20:01 +0530

    deleted unwanted codes

--- src/app/model/approvedRequestForEvent.ts
+++
@@ -1,9 +0,0 @@
-export interface approvedRequestForEvent {
-  requestId: number;
-  tanNumber: string;
-  noOfParticipants: number;
-  requestDate: string;
-  justification: string;
-  requestedBy: string;
-  department: string;
-}
